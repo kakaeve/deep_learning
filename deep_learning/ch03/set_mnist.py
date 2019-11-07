ae370e89ac575c2718f6567dd8689ced5f5ff4a3
kakaeve <zkzkdlqm@gmail.com>
2019-11-07 17:02:56 +0900

    test

--- deep_learning/ch03/set_mnist.py
+++ deep_learning/ch03/set_mnist.py
@@ -2,3 +2,11 @@ import sys,os
 sys.path.append(os.pardir)
 from dataset.minst import load_mnist
 
+(x_train, t_train), (x_test, t_test) = |
+	load_mnist(flatten = True, nomalize=False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(x_test.shape)
+	
